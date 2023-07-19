eafc881f399e9f9f9c3372eb9f67c63a783e6de5
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-07-19 16:04:10 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,19 +4,18 @@ var ground, invisibleGround, groundImage;
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   trex_collided = loadImage("trex_collided.png");
-  groundImage = loadImage("ground2.png")
+ 
 }
 
 function setup() {
-  createCanvas(600, 200);
+
   
   //crie um sprite de trex
   trex = createSprite(50,180,20,50);
   trex.addAnimation("running", trex_running);
   
   //adicione dimensão e posição ao trex
-  trex.scale = 0.5;
-  trex.x = 50
+ 
   
   //crie um sprite ground (solo)
   ground = createSprite(200,180,400,20);
@@ -35,14 +34,11 @@ function draw() {
   }
   
   //pular quando a tecla espaço for pressionada
-  if(keyDown("space")) {
-    trex.velocityY = -10;
-  }
-  
+
   trex.velocityY = trex.velocityY + 0.8
   
  
  //impedir que o trex caia
-  trex.collide(ground);
+  
   drawSprites();
 }
